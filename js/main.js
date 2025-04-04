a78af7a22577bc7b47ac68b55b889d913f3fc105
jey <krivosheyev.evheni@gmail.com>
2025-04-04 14:59:46 +0300

    js page

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
 const pythonButton = document.querySelector(".python-button")
 const javaScriptButton = document.querySelector(".javascript-button")
+const homePageButton = document.querySelector(".home-button")
 
-pythonButton.addEventListener("click", () => {
-    window.location.href = "/pages/pythonPage.html"
-})
+if (pythonButton) {
+    pythonButton.addEventListener("click", () => {
+        window.location.href = "/pages/pythonPage.html";
+    });
+}
 
-javaScriptButton.addEventListener("click", () => {
-    window.location.href = "/pages/javaScriptPage.html"
-})
+if (javaScriptButton){
+    javaScriptButton.addEventListener("click", () => {
+        window.location.href = "/pages/javaScriptPage.html"
+    })
+}
+
+if (homePageButton){
+    homePageButton.addEventListener("click", () => {
+        window.location.href = "/index.html"
+    })
+}
+

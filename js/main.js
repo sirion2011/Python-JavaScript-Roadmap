46aced637ac3764fbe4dc540ac26b70462012762
jey <krivosheyev.evheni@gmail.com>
2025-05-14 01:45:42 +0300

    day2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const pythonButton = document.querySelector(".python-button")
 const javaScriptButton = document.querySelector(".javascript-button")
 const homePageButton = document.querySelector(".home-button")
+const backScreenButton = document.querySelector(".back-button")
+const homeScreenText = document.querySelector(".page-title")
 
 if (pythonButton) {
     pythonButton.addEventListener("click", () => {
@@ -20,3 +22,16 @@ if (homePageButton){
     })
 }
 
+if (backScreenButton){
+    backScreenButton.addEventListener("click", () => {
+        window.location.href = "/pages/pythonPage.html"
+    })
+}
+
+if (homeScreenText){
+    homeScreenText.title = "Главный экран"
+    homeScreenText.style.cursor = "pointer"
+    homeScreenText.addEventListener("click", () => {
+        window.location.href = "/index.html"
+    })
+}
